5b73d1892100f9426d6f72a022ce7dd95ab6362d
Nguyễn Văn Huy <huynv@geso.us>
2026-05-05 09:56:19 +0700

    Fix Folder antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import ThemeProvider from './app/providers/theme/ThemeProvider';
-import AntdProvider from './app/providers/AntdProvider';
+import AntdProvider from './app/providers/antd/AntdProvider';
 import { router } from './app/router/routes';
 
 function App() {
